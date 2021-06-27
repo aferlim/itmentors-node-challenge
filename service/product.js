39800870ad201062f1c7274f0698406c3b1f9ed0
Andre Ferreira Lima <andre.lima@grupoltm.com.br>
2021-06-27 17:26:51 -0300

    adicionando laboratório 2

--- service/product.js
+++ service/product.js
@@ -4,15 +4,23 @@ const { v4: uuidv4 } = require('uuid');
 const fs = require('fs');
 const repoFile = './database/data.json';
 
-
 module.exports.age = 38;
-
 
 module.exports.AddProduct = (product, callback) => {
 
     var products = JSON.parse(fs.readFileSync(repoFile));
 
     products.push(product);
+
+    fs.writeFile(repoFile, JSON.stringify(products), callback);
+
+}
+
+module.exports.AddList = (list, callback) => {
+
+    var products = JSON.parse(fs.readFileSync(repoFile));
+
+    list.map(prod => products.push(prod));
 
     fs.writeFile(repoFile, JSON.stringify(products), callback);
 
@@ -26,11 +34,12 @@ module.exports.ListAll = () => {
 
 }
 
-module.exports.SetProduct = (title, description, price, stock) => {
+module.exports.SetProduct = (title, description, price, stock, image) => {
     return {
         id: uuidv4(),
         name: title,
         description: description,
+        image, 
         price: price,
         stock: stock
     }
